5028358bca572a74dfbfda0d149de648eb4a3ae6
TheLegendLord <fabcic009@gmail.com>
2026-01-12 21:57:51 +0100

    route change

--- src/visualization_service/main.py
+++ src/visualization_service/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from visualization_service.api.players import router as players_router
 from visualization_service.api.standings import router as standings_router
 
-app = FastAPI(title="Sportly Visualization Service", root_path="/api/charts")
+app = FastAPI(title="Sportly Visualization Service")
 
 app.include_router(players_router)
 app.include_router(standings_router)
